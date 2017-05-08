6d8a50dcc70c7f83d54e57072f05a91ec657b11e
cgianmarco <c.giammy95@gmail.com>
2017-05-08 12:12:01 +0200

    changed script

--- python/script.py
+++ python/script.py
@@ -7,10 +7,10 @@
 
 import subprocess
 
-movie_title = "Titanic"
+movie_title = "Arrival"
 
 # Execute YTFS
-test = subprocess.Popen(["java","-jar","YTFS.jar","--limit","20",movie_title], stdout=subprocess.PIPE)
+test = subprocess.Popen(["java","-jar","YTFS.jar","--limit","50",movie_title], stdout=subprocess.PIPE)
 
 # Get output
 output = test.communicate()[0]
@@ -46,14 +46,14 @@ import urllib, urllib2
 import json
 
 comments = []
-# videoId = "l9PxOanFjxQ"
+# videoIds = ["kVrqfYjkTdQ"]
 maxResults = 100
 
 
 for videoId in videoIds:
 
 	# Youtube query
-	query = "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=" + str(videoId) + "&maxResults=" + str(maxResults) + "&key=AIzaSyBHWLQQO7jtOu1i49pgKL4h0uupCDjK-Iw"
+	query = "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet,replies&videoId=" + str(videoId) + "&maxResults=" + str(maxResults) + "&key=AIzaSyBHWLQQO7jtOu1i49pgKL4h0uupCDjK-Iw"
 	print query
 
 
@@ -68,16 +68,22 @@ for videoId in videoIds:
 		# From string to JSON
 		response = json.loads(response)
 
+
 		# Get comments
 		for i in range(len(response['items'])):
 			comments.append(response['items'][i]['snippet']['topLevelComment']['snippet']['textDisplay'])
+			if 'replies' in response['items'][i]:
+				for j in range(len(response['items'][i]['replies']['comments'])):
+					comments.append(response['items'][i]['replies']['comments'][j]['snippet']['textDisplay'])
+
 
 
 # Save comments to file
-file = open('comments.txt', 'w')
+file = open('comments_' + str(movie_title) + '.txt', 'w')
 for item in comments:
 	file.write("%s\n" % item.encode('utf-8'))
 
 	
 print "Number of saved comments: " + str(len(comments))
 
+# {u'snippet': {u'totalReplyCount': 1, u'canReply': False, u'topLevelComment': {u'snippet': {u'authorChannelUrl': u'http://www.youtube.com/channel/UCJy9frQKoo0vf55AMDHacKw', u'authorDisplayName': u'buguedsteve55', u'updatedAt': u'2017-05-05T13:06:37.000Z', u'videoId': u'l9PxOanFjxQ', u'publishedAt': u'2017-05-05T13:06:37.000Z', u'viewerRating': u'none', u'authorChannelId': {u'value': u'UCJy9frQKoo0vf55AMDHacKw'}, u'canRate': False, u'textOriginal': u'what is the song ID', u'likeCount': 0, u'authorProfileImageUrl': u'https://yt3.ggpht.com/-O3ogszEdZaY/AAAAAAAAAAI/AAAAAAAAAAA/rB9Lzv7kYgY/s28-c-k-no-mo-rj-c0xffffff/photo.jpg', u'textDisplay': u'what is the song ID'}, u'kind': u'youtube#comment', u'etag': u'"m2yskBQFythfE4irbTIeOgYYfBU/h0M9k97yhd_Yqx8DtZZ-oL8l7mM"', u'id': u'z12le5oy1qyfjry2t23att3aqvj3tbjte'}, u'videoId': u'l9PxOanFjxQ', u'isPublic': True}, u'kind': u'youtube#commentThread', u'etag': u'"m2yskBQFythfE4irbTIeOgYYfBU/_BjhspLOV3ey9q3Paeb7-k2dZJw"', u'id': u'z12le5oy1qyfjry2t23att3aqvj3tbjte'}
